995d5db98a2482f0b634043ccc22d4779f9670a4
FilipaMerino <filipaam@outlook.com>
2025-07-01 15:45:53 +0100

    footer

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,6 +1,14 @@
 export default function Footer() {
   return(
-    <>
-    </>
+    <section>
+      <div className='flex items-center justify-center'>
+        <div>
+          
+        </div>
+        <p className='text-sm!'>
+          © 2022 ExtendedAway, Inc All Rights Reserved.
+        </p>
+      </div>
+    </section>
   )
 }
