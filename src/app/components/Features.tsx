de9d734c3ddbdb207a5a77614c7070ab94282033
FilipaMerino <filipaam@outlook.com>
2025-07-01 15:39:46 +0100

    feature section

---
+++ src/app/components/Features.tsx
@@ -0,0 +1,53 @@
+'use client';
+import Image from "next/image";
+import styles from './Feature.module.css';
+
+
+export default function FeaturesSection() {
+  return (
+    <section className='bg-lightGreen flex flex-col justify-center items-center'>
+      <div>
+        <h2>All you need to enjoy some peaceful days off</h2>
+      </div>
+
+      <div className='flex justify-around'>
+        <div className="featureSection">
+          <Image
+            src='/house.png'
+            width={70}
+            height={70}
+            alt="house"
+          />
+          <h4>Home Safety</h4>
+          <p>
+          Worried about your house getting robbed while you are away? Your trusted friends will be there to make it look habited.
+          </p>
+        </div>
+         <div className="featureSection">
+          <Image
+            src='/clipboard.png'
+            width={70}
+            height={70}
+            alt="clipboard"
+          />
+          <h4>Dashboard</h4>
+          <p>
+            You will have a clear picture of what needs to be done, and what is already done. No task left behind!
+          </p>
+        </div>
+         <div className="featureSection">
+          <Image
+            src='/paw.png'
+            width={70}
+            height={70}
+            alt="paw"
+          />
+          <h4>Animal Care</h4>
+          <p>
+            Your pets will never feel alone again. Your caretakers will make sure to feed and comfort them, in the days you choose.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import FeaturesSection from "./components/Features";
 import HeroSection from "./components/sections/HeroSection";
+import MissionStatement from "./components/sections/MissionStatement";
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
+      <MissionStatement />
+      <FeaturesSection />
     </div>
   );
 }
