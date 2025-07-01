5b1f282b4493e724b609ba02eeeddf5d9a615ca0
FilipaMerino <filipaam@outlook.com>
2025-07-01 15:39:55 +0100

    mission statement component

--- src/app/components/sections/MissionStatement.tsx
+++ src/app/components/sections/MissionStatement.tsx
@@ -1,7 +1,13 @@
 export default function MissionStatement() {
   return (
-    <section>
-
+    <section className='flex items-center h-[25rem]'>
+      <div className='w-[20%]'>
+        <h2>About us and our mission</h2>
+      </div>
+      <div className='w-[80%]'>
+        <p>At ExtendedAway, we think about our clients as if they were our own friends. Our mission is to provide a simple, intuitive way for Homeowners and Caretakers to interact and get things done, fast.
+          Created by a group of 4 young entrepreneurs, ExtendedAway, has been operating worldwide for some days now. Whether you are taking a trip, and need a friend to take care of your house, or you are that friend that is coming to help, ExtendedAway is the app for you.</p>
+      </div>
     </section>
    
   );
